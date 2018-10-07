d7bf0808f8c7d89fd0aa7ca8866c697a186db7ac
Dana Palmer <roiai4@hotmail.com>
2018-10-07 06:10:23 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,37 +1,27 @@
 require 'pry'
-# Write your code here.
-
-names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
    "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(names)
-    names.map do |list| 
-    "Hello, my name is #{list}."
+    names.map { |name| badge_maker(name) }
 end
-end
- 
- rooms = [1, 2, 3, 4, 5, 6, 7]
- 
+
  def assign_rooms(rooms)
    combo = []
-   rooms.each_with_index do |name, index|
-     #binding.pry
-     combo << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+   rooms.each_with_index do |name, i|
+     combo << "Hello, #{name}! You'll be assigned to room #{i + 1}!"
  end
- combo 
+ combo
  end
- 
- def printer(names)
-   batch_badge_creator(names).each do |part_one|
-     puts part_one
-   end
-   
-   assign_rooms(names).each do |part_two|
-     puts part_two
-   end
-   
- end
-   
+
+ def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
